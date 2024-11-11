1773249a7617b9107749063b09690d87297c21b4
G4ngst4C0d3 <andersonpleite2022@gmail.com>
2024-11-10 22:28:58 -0300

    fix:MAR-7 redimensionamento da logo e do nome do projeto, problrmas de alinhamento com o tamanho da imagem

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -10,7 +10,7 @@ const LoginScreen = () => {
 
   const handleLogin = () => {
     if (!password) {
-      setErrorMessage('Senha obrigatoria');
+      setErrorMessage('Senha obrigatória');
     } else {
       setErrorMessage('');
       Alert.alert('Logando...', `E-mail: ${email}`);
@@ -19,18 +19,18 @@ const LoginScreen = () => {
 
   return (
     <SafeAreaView style={{
-      backgroundColor:"red"
-    }} className="flex max-w-xl px-4 h-full justify-center items-center">
-      {/* <Image source={require('./assets/marquei-logo.png')} className="w-36 h-24 mb-8" resizeMode="contain" /> */}
-      <View className="flex-row justify-center items-center mb-6 ml-6">
-        <Image source={require('../../assets/images/marqueiLogo.png')} className="w-36 h-24 mb-8" resizeMode='contain'></Image>
-        <Text className="text-xl text-black text-center  font-bold">Marquei</Text>
+      backgroundColor:""
+    }} className="flex px-4 mr-5 ml-0 h-full justify-center items-center">
+      <View className="flex-row justify-around items-center">
+        <Image source={require('../../assets/images/marqueiLogo.png')} className="z-0 ml-4 mb-8" resizeMode='contain'></Image>
+        <Text className="z-50 text-6xl ml-0 mr-14 mb-10 text-black text-center font-bold">Marquei</Text>
       </View>
+      
       <TextInput
         placeholder="E-mail"
         value={email}
         onChangeText={setEmail}
-        className="w-full border border-teal-500 rounded-full px-15 p-2 mb-3"
+        className="w-full border border-teal-500 rounded-full px-15 p-2 mt-0 mb-3"
       />
       <View className='w-full'> 
         <TextInput
@@ -48,11 +48,11 @@ const LoginScreen = () => {
       </View>
       {errorMessage ? <Text className="text-red-500 text-sm mb-2 p-2">{errorMessage}</Text> : null}
 
-      <Button  onPress={handleLogin} className=" w-full bg-teal-500 border border-teal-500 px-9  rounded-full justify-center items-center mb-3">
+      <Button  onPress={handleLogin} className=" w-full bg-teal-500 border px-9  rounded-full justify-center items-center mb-3">
         <Text className="text-white text-lg font-bold px-10 ">Acessar</Text>
       </Button>
       <Button onPress={() => Alert.alert('Criar Conta')} className= "w-full bg-teal-500 border border-teal-500 px-9 rounded-full justify-center items-center mb-3">
-        <Text className="text-white text-lg  px-10 mb-2 font-bold">Criar Conta</Text>
+        <Text className="text-white text-lg  px-10  font-bold">Criar Conta</Text>
       </Button>
       <View className="flex-row justify-around w-40 rounded-full mt-7 mb-2">
         <FontAwesome name="google" size={24} color="#4285F4" />
