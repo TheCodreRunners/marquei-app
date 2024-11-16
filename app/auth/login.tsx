dd8e3bc1b176356cf7be9ebfa2b719fbd6829993
G4ngst4C0d3 <andersonpleite2022@gmail.com>
2024-11-16 01:37:04 -0300

    fix MAR-7-login Ajuste do logotipo

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -6,7 +6,7 @@ const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState(''); 
 
   const handleLogin = () => {
     if (!password) {
@@ -20,44 +20,45 @@ const LoginScreen = () => {
   return (
     <SafeAreaView style={{
       backgroundColor:""
-    }} className="flex px-4 mr-5 ml-0 h-full justify-center items-center">
-      <View className="flex-row justify-around items-center">
-        <Image source={require('../../assets/images/marqueiLogo.png')} className="z-0 ml-4 mb-8" resizeMode='contain'></Image>
-        <Text className="z-50 text-6xl ml-0 mr-14 mb-10 text-black text-center font-bold">Marquei</Text>
+    }} className="flex  px-4 h-full  items-center">
+      <View className="flex-row mr-2 mb-30 mt-14 items-center">
+        <Image source={require('../../assets/images/marquei_logo.png')} className="z-0 ml-4 mt-10" resizeMode='contain'></Image>
       </View>
       
       <TextInput
         placeholder="E-mail"
         value={email}
         onChangeText={setEmail}
-        className="w-full border border-teal-500 rounded-full px-15 p-2 mt-0 mb-3"
+        className="w-full border border-teal-500 justify-center rounded-full px-15 p-3 mt-40 mb-3 "
       />
-      <View className='w-full'> 
+      <View className='w-full '> 
         <TextInput
           placeholder="Senha"
           value={password}
           onChangeText={setPassword}
           secureTextEntry={!showPassword}
-          className=" w-full border  border-teal-500 rounded-full px-15 p-2 mb-3"
+          className=" w-full border justify-center border-teal-500 rounded-full px-15 mt-1 p-3 mb-3"
         />
+        
         <TouchableOpacity
-          onPress={() => setShowPassword(!showPassword)}
-          className="absolute right-3 top-2">
-          <FontAwesome name={showPassword ? 'eye' : 'eye-slash'} size={24} color="gray" />
-        </TouchableOpacity>
-      </View>
-      {errorMessage ? <Text className="text-red-500 text-sm mb-2 p-2">{errorMessage}</Text> : null}
+            onPress={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-2">
+            <FontAwesome name={showPassword ? 'eye' : 'eye-slash'} size={24} color="gray"/>
+          </TouchableOpacity>
+        </View>
+        {errorMessage ? <Text className="text-red-500 text-sm mb-2 p-3">{errorMessage}</Text> : null}
 
-      <Button  onPress={handleLogin} className=" w-full bg-teal-500 border px-9  rounded-full justify-center items-center mb-3">
-        <Text className="text-white text-lg font-bold px-10 ">Acessar</Text>
-      </Button>
-      <Button onPress={() => Alert.alert('Criar Conta')} className= "w-full bg-teal-500 border border-teal-500 px-9 rounded-full justify-center items-center mb-3">
-        <Text className="text-white text-lg  px-10  font-bold">Criar Conta</Text>
-      </Button>
-      <View className="flex-row justify-around w-40 rounded-full mt-7 mb-2">
-        <FontAwesome name="google" size={24} color="#4285F4" />
-        <FontAwesome name="facebook" size={24} color="#3b5998" />
-      </View>
+        <Button  onPress={handleLogin} className=" w-full bg-teal-500 border px-9 mt-1 rounded-full justify-center items-center mb-3">
+          <Text className="text-white text-lg font-bold px-10 ">Acessar</Text>
+        </Button>
+        <Button onPress={() => Alert.alert('Criar Conta')} className= "w-full bg-teal-500 mt-1 border border-teal-500 px-9 rounded-full justify-center items-center mb-3">
+          <Text className="text-white text-lg  px-10  font-bold">Criar Conta</Text>
+        </Button>
+        <View className="flex-row justify-around w-40 rounded-full mt-7 mb-2">
+          <FontAwesome name="google" size={24} color="#4285F4" />
+          <FontAwesome name="facebook" size={24} color="#3b5998" />
+        </View>
+      
     </SafeAreaView>
   );
 };
